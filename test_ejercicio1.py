342c1c36fb0dd3de3c72c4a884f2484f239f46d5
FedericoNery <fedegnery@gmail.com>
2017-05-25 12:38:54 -0300

    Se realizan y se pasan correctamente todos los test unitarios

--- test_ejercicio1.py
+++ test_ejercicio1.py
@@ -14,27 +14,27 @@ print(ejercicio1(331,[1,2,3,7,147,331,518])) # [1,331]
 
 class StringsTestCase(unittest.TestCase):
     def test_is_vacio_un_divisor(self):
-        self.assertIsNone(ejercicio1.generarListaDivisores("",[1,2,3],"Es vacio"))
+        self.assertEquals(ejercicio1.generarListaDivisores("",[1,2,3]),[])
 
     def test_is_numNegativo_un_divisor(self):
-        self.assertIsNone(ejercicio1.generarListaDivisores(-1,[1,2,3]),"Es vacio")
+        self.assertEquals(ejercicio1.generarListaDivisores(-1,[1,2,3]),[])
 
     def test_is_cero_un_divisor(self):
-        self.assertIsNone(ejercicio1.generarListaDivisores(0,[1,2,3]),"Es vacio")
+        self.assertEquals(ejercicio1.generarListaDivisores(0,[1,2,3]),[])
 
     def test_is_cero_un_divisor_de_lista_sin_multiplos(self):
-        self.assertIsNone(ejercicio1.generarListaDivisores(0,[]),"Es vacio")
+        self.assertEquals(ejercicio1.generarListaDivisores(0,[]),[])
 
     def test_is_uno_un_divisor(self):
-        self.assertIn(ejercicio1.generarListaDivisores(1,[1,2]),1,"Contiene Uno")
+        self.assertEquals(ejercicio1.generarListaDivisores(1,[1,2]),[1])
 
     def test_is_dos_un_divisor(self):
-        self.assertEquals(ejercicio1.generarListaDivisores(2,[1,-2]))
+        self.assertEquals(ejercicio1.generarListaDivisores(2,[1,-2]),[1,-2])
 
     def test_is_ocho_un_divisor(self):
-        self.assertEquals(ejercicio1.generarListaDivisores(8,[1,7,2,-4,6,9]))
+        self.assertEquals(ejercicio1.generarListaDivisores(8,[1,7,2,-4,6,9]),[1,2,-4])
 
     def test_is_trescientostreintayuno_un_divisor(self):
-        self.assertEquals(ejercicio1.generarListaDivisores(331,[1,2,3,7,147,331,518]))
+        self.assertEquals(ejercicio1.generarListaDivisores(331,[1,2,3,7,147,331,518]),[1,331])
 
 unittest
