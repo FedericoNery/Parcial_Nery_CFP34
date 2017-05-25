839cad9eb203b65d4a25cda29b7a4c4476774261
FedericoNery <fedegnery@gmail.com>
2017-05-25 11:56:41 -0300

    Se logra realizar las pruebas unitarias para el ejercicio 2 y son "pasadas" todas las pruebas programadas

--- test_ejercicio2.py
+++ test_ejercicio2.py
@@ -12,29 +12,29 @@ print(ejercicio2('/home/user/listado',[])) # False
 print(ejercicio2('',[])) # False
 """
 
-class ejercicio2TestCase(unittest.testCase):
+class ejercicio2TestCase(unittest.TestCase):
     def test_is__home_user_listado_txt__mp3_wav_mpeg__un_formato_valido(self):
-        self.assertFalse(ejercicio2.is_validarExtension('/home/user/listado.txt',['mp3','wav','mpeg']))
+        self.assertFalse(ejercicio2.validarExtension('/home/user/listado.txt',['mp3','wav','mpeg']),"Es Falso")
 
     def test_is__home_user_listado_txt__mp3_wav_mpeg_txt__un_formato_valido(self):
-        self.assertTrue(ejercicio2.is_validarExtension('/home/user/listado.txt',['mp3','wav','mpeg','txt']))
+        self.assertTrue(ejercicio2.validarExtension('/home/user/listado.txt',['mp3','wav','mpeg','txt']),"Es Verdadero")
 
     def test_is__home_user_listado_txt__mp3_wav_mpeg_TXT__un_formato_valido(self):
-        self.assertTrue(ejercicio2.is_validarExtension('/home/user/listado.txt',['mp3','wav','mpeg','TXT']))
+        self.assertTrue(ejercicio2.validarExtension('/home/user/listado.txt',['mp3','wav','mpeg','TXT']),"Es Verdadero")
 
     def test_is__home_user_listado_tXt__mp3_wav_mpeg_TXT__un_formato_valido(self):
-        self.assertTrue(ejercicio2.is_validarExtension('/home/user/listado.tXt',['mp3','wav','mpeg','TXT']))
+        self.assertTrue(ejercicio2.validarExtension('/home/user/listado.tXt',['mp3','wav','mpeg','TXT']),"Es Verdadero")
 
     def test_is__home_user_listado_txt__txt__un_formato_valido(self):
-        self.assertTrue(ejercicio2.is_validarExtension('/home/user/listado.txt',['txt']))
+        self.assertTrue(ejercicio2.validarExtension('/home/user/listado.txt',['txt']),"Es Verdadero")
 
     def test_is__home_user_listado__mp3_wav_mpeg_txt__un_formato_valido(self):
-        self.assertFalse(ejercicio2.is_validarExtension('/home/user/listado',['mp3','wav','mpeg','txt'))
+        self.assertFalse(ejercicio2.validarExtension('/home/user/listado',['mp3','wav','mpeg','txt']),"Es Falso")
 
     def test_is__home_user_listado__vacio__un_formato_valido(self):
-        self.assertFalse(ejercicio2.is_validarExtension('/home/user/listado',[]))
+        self.assertFalse(ejercicio2.validarExtension('/home/user/listado',[]),"Es Falso")
 
     def test_is__vacio__vacio__un_formato_valido(self):
-        self.assertFalse(ejercicio2.is_validarExtension('',[]))
+        self.assertFalse(ejercicio2.validarExtension('',[]),"Es Falso")
 
-
+unittest
