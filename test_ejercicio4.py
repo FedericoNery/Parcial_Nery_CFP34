077a10f3b1836dbaec22dd740da2ea5716ef9ef9
FedericoNery <fedegnery@gmail.com>
2017-05-30 19:07:40 -0300

    Se cambian los nombres de las funciones de las pruebas unitarias.

--- test_ejercicio4.py
+++ test_ejercicio4.py
@@ -26,13 +26,13 @@ class DeterminoGanadorDeLaLigaTestCase(unittest.TestCase):
     def test_devuelveCaracter_a_SiElArgumentoEsUnSoloPartido(self):
         self.assertEquals(ejercicio4.calcularGanadorDeLiga([("a",1,"b",0)]),'a')
 
-    def test_is__a1_b1__a1_c1__c1_b1__seDeterminaCampeon(self):
+    def test_devuelve_a_b_c_SiElArgumentoSonTresPartidos(self):
         self.assertIn(ejercicio4.calcularGanadorDeLiga([("a",1,"b",1),("a",1,"c",1),("c",1,"b",1)]),['c','b','a'])
 
-    def test_is__a1_b0__a1_c2__c3_b0__seDeterminaCampeon(self):
+    def test_devuelve_c_SiElArgumentoSonTresPartidos(self):
         self.assertEquals(ejercicio4.calcularGanadorDeLiga([("a",1,"b",0),("a",1,"c",2),("c",3,"b",0)]),'c')
 
-    def test_is__a1_bmenos2__a1_c1__c1_b1__d1_a9__seDeterminaCampeon(self):
+    def test_devuelve_a_SiElArgumentoSonCuatroPartidos(self):
         self.assertEquals(ejercicio4.calcularGanadorDeLiga([("a",1,"b",-2),("a",1,"c",1),("c",1,"b",1),("d",1,"a",9)]),'a')
 
 unittest
