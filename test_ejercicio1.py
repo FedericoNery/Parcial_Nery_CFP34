2e4b7efd3b40ead4afb46ac91fc1ad00e35aae13
FedericoNery <fedegnery@gmail.com>
2017-05-30 18:12:07 -0300

    Se cambian los nombres a las funciones de las pruebas unitarias

--- test_ejercicio1.py
+++ test_ejercicio1.py
@@ -12,29 +12,30 @@ print(ejercicio1(331,[1,2,3,7,147,331,518])) # [1,331]
 """
 
 
-class StringsTestCase(unittest.TestCase):
-    def test_is_vacio_un_divisor(self):
+class GenerarListaDivisoresTestCase(unittest.TestCase):
+
+    def test_devuelveListaVaciaSiElDividendoEsVacio(self):
         self.assertEquals(ejercicio1.generarListaDivisores("",[1,2,3]),[])
 
-    def test_is_numNegativo_un_divisor(self):
+    def test_devuelveListaVaciaSiElDividendoEsUnNumeroNegativo(self):
         self.assertEquals(ejercicio1.generarListaDivisores(-1,[1,2,3]),[])
 
-    def test_is_cero_un_divisor(self):
+    def test_devuelveListaVaciaSiElDividendoEsCero(self):
         self.assertEquals(ejercicio1.generarListaDivisores(0,[1,2,3]),[])
 
-    def test_is_cero_un_divisor_de_lista_sin_multiplos(self):
+    def test_devuelveListaVaciaSiElDividendoEsCeroYLaListaDeNumeradoresEsVacia(self):
         self.assertEquals(ejercicio1.generarListaDivisores(0,[]),[])
 
-    def test_is_uno_un_divisor(self):
+    def test_devuelveUnoSiElDividendoEsUnoYLosNumeradoresSonUnoYDos(self):
         self.assertEquals(ejercicio1.generarListaDivisores(1,[1,2]),[1])
 
-    def test_is_dos_un_divisor(self):
+    def test_devuelveUnoyMenosDosSiElDividendoEsDosYLosNumeradoresSonUnoYMenosDos(self):
         self.assertEquals(ejercicio1.generarListaDivisores(2,[1,-2]),[1,-2])
 
-    def test_is_ocho_un_divisor(self):
+    def test_devuelveUnoDosYMenosCuatroSiElDividendoEsOchoYLosNumeradoresSonUnoSieteDosMenosCuatroSeisNueve(self):
         self.assertEquals(ejercicio1.generarListaDivisores(8,[1,7,2,-4,6,9]),[1,2,-4])
 
-    def test_is_trescientostreintayuno_un_divisor(self):
+    def test_devuelveUnoYTrescientosTreintaYUnoSiElDividendoEsTrescientosTreintaYUno(self):
         self.assertEquals(ejercicio1.generarListaDivisores(331,[1,2,3,7,147,331,518]),[1,331])
 
 unittest
